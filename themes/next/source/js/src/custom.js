4604b6770b8b12dab5fb2772fe1c65e705ebf438
lewis.liu <1019175915@qq.com>
2018-06-01 01:24:23 +0800

    update js/ css code

--- themes/next/source/js/src/custom.js
+++ themes/next/source/js/src/custom.js
@@ -5,17 +5,25 @@ function randomColor() {
 
 /* 鼠标点击文字特效 */
 var a_idx = 0;
+var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善",
+			"老哥稳", "带我飞", "厉害了word哥", "扎心了老铁", "蓝瘦香菇", "还有这种操作?", "就是有这种操作",
+			"皮皮虾我们走", "笑到猪叫", "石乐志", "不存在的", "黑车!", "我要下车!", "他还只是个孩子", "请不要放过他",
+			"惊不惊喜?", "意不意外?", "我有一个大胆的想法", "你的良心不会痛吗", "你心里就没点b数吗", "没有,我很膨胀",
+			"秀", "天秀", "陈独秀", "蒂花之秀", "造化钟神秀", "我去买几个橘子", "你就站在此地", "不要走动",
+			"我可能读了假书", "贫穷限制了我的想象力", "打call", "当然是选择原谅她啊", "你有freestyle吗",
+			"不知妻美刘强东", "悔创阿里杰克马", "一无所有王健林", "普通家庭马化腾",
+			"别点了", "求求你别点了", "你还点", "你再点!", "有本事继续点!", "你厉害", "我投翔",
+			"w(·Д·)w", "(#`O′)", "（/TДT)/", "┭┮﹏┭┮", "_(:3」∠)_");
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
         y = e.pageY;
         $i.css({
-            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+            "z-index": 9999,
             "top": y - 20,
-            "left": x,
+            "left": x + 1,
             "position": "absolute",
             "font-weight": "bold",
             "color": randomColor()
@@ -36,13 +44,16 @@ jQuery(document).ready(function($) {
 var a_click = 1;
 var avatar_plug = 0;
 jQuery(document).ready(function($) {
+	/* 点击频率，点击几次就换挂件 */
 	var frequency = 3;
+	/* 头像挂件数量 */
+	var plug_count = 38;
 	$(".site-avatar-plug-bilibili").click(function(e) {
 		if (a_click % frequency === 0) {
 			avatar_plug ++;
 			$(".site-avatar-plug-bilibili").attr("src","/images/avatar-plug/bilibili_" + avatar_plug + ".png");
 		}		
-		if (avatar_plug === 38) {
+		if (avatar_plug === plug_count) {
 			avatar_plug = 0;
 		}
 		$(".site-avatar-plug-bilibili").attr("alt","再点击" + (frequency - a_click % frequency) + "次头像试试看~~");
