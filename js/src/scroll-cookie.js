d9b1717075eee0767e90de10097be8f4257827eb
Lewis.Liu <1019175915@qq.com>
2019-08-21 00:18:52 +0800

    Site updated: 2019-08-21 00:17:43

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Jun 07 2019 22:13:53 GMT+0800 (中国标准时间)
+// build time:Wed Aug 21 2019 00:14:58 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
