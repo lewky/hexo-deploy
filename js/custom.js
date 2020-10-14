01a1d7270a326bb60bfc9b571cae934ff611b77d
Lewis.Liu <1019175915@qq.com>
2020-10-14 14:14:49 +0800

    migrate blog from hexo

--- js/custom.js
+++ js/custom.js
@@ -4,8 +4,9 @@ if(window.console) {
 	if(cons) { 
 		cons.group("O(∩_∩)O哈喽！");
 		cons.info("这位看代码的童鞋，不如留下你的友链来一起玩耍吧！"); 
-		cons.info("如果有发现什么bug，还请帮忙留言提醒，或者到我的GitHub项目里提交issue哦~ %c(https://github.com/lewky/hexo-theme-beep)","color:#a517ef;font-weight:bold;");
-		cons.log("%cYulin Lewis's Blog", "background-image:-webkit-gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );color:transparent;-webkit-background-clip: text;font-size:5em;");
+		cons.log("%cYulin Lewis's Blog", "background-image: linear-gradient(#063053, #395873, #5c7c99);font-size: 2rem;");
+		cons.info("==============================此乃分界线==============================");
+		cons.log("  █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗\n▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝\n▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗\n░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║\n░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝\n ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝\n ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░\n ░ ░    ░░░ ░ ░ ░        ░ ░░ ░\n          ░     ░ ░      ░  ░\n")
 		cons.groupEnd();
 	} 
 }
